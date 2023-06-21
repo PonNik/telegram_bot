9f566ff1bfc4b980c5193ba30b71d5588fdf99c6
Ponosov Nikita <93863311+PonNik@users.noreply.github.com>
2023-06-21 15:25:48 +0300

    Добавил приветственное сообщение и вывод кнопки

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from aiogram.utils import executor
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from rnd import get_rnd_message
+from rnd import get_rnd_message, hello_msg
 
-TOKEN = 'ваш_токен'
+TOKEN = '5689961299:AAF_cXmUhz-JtWuyF8n8xZzL_kJaNUVFv_4'
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -43,7 +43,16 @@ async def get_video(message: types.Message):
     button = types.InlineKeyboardButton(text="ПОДРОБНЫЕ УСЛОВИЯ", url="https://github.com")
     keyboard.add(button)
     await bot.send_video_note(message.chat.id, video_note, reply_markup=keyboard)
-    await get_video_note(message)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button = types.InlineKeyboardButton(text="❗️❗️Я НЕ БОТ❗️❗️", callback_data='f')
+    keyboard.add(button)
+    await message.answer(hello_msg['message'], reply_markup=keyboard, parse_mode=types.ParseMode.HTML)
+
+@dp.message_handler(content_types=['text'])
+async def not_bot(message: types.Message):
+    if message.text == "❗️❗️Я НЕ БОТ❗️❗️":
+        await bot.send_message(message.chat.id, "✅СПАСИБО! ПОДТВЕРЖДЕНИЕ ПОЛУЧЕНО✅")
+        await get_video_note(message)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
